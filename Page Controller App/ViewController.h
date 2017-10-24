18c5784239166340c347acf956871d984638f34f
Amir J <amir.jahan@gmail.com>
2017-10-23 17:01:12 -0700

    first commit

--- Page Controller App/ViewController.h
+++ Page Controller App/ViewController.h
@@ -1,14 +1,8 @@
-//
-//  ViewController.h
-//  Page Controller App
-//
-//  Created by Amir on 10/23/17.
-//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
-//
+
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIScrollViewDelegate>
 
 
 @end
--- Page Controller App/ViewController.m
+++ Page Controller App/ViewController.m
@@ -1,28 +1,113 @@
-//
-//  ViewController.m
-//  Page Controller App
-//
-//  Created by Amir on 10/23/17.
-//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
-//
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIPageControl *myPageControl;
+
+@property (weak, nonatomic) IBOutlet UIScrollView *myScrollView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+-(void)loadScrollViewWithPage:(UIView *)page
+{
+    NSUInteger pageCount = [[_myScrollView subviews] count] - 1;
+    
+    CGRect bounds = _myScrollView.bounds;
+    bounds.origin.x = bounds.size.width * pageCount;
+    bounds.origin.y = 0;
+    page.frame = bounds;
+    [_myScrollView addSubview:page];
+}
+
+-(void) createPages
+{
+    CGRect pageRect = _myScrollView.frame;
+    
+    UIView *page1 = [[UIView alloc] initWithFrame:pageRect];
+    page1.backgroundColor = [UIColor redColor];
+    
+    UIView *page2 = [[UIView alloc] initWithFrame:pageRect];
+    page2.backgroundColor = [UIColor blueColor];
+    
+    UIView *page3 = [[UIView alloc] initWithFrame:pageRect];
+    page3.backgroundColor = [UIColor greenColor];
+    
+    UIView *page4 = [[UIView alloc] initWithFrame:pageRect];
+    page4.backgroundColor = [UIColor darkGrayColor];
+    
+    //add them to scrooll view
+    [self loadScrollViewWithPage:page1];
+    [self loadScrollViewWithPage:page2];
+    [self loadScrollViewWithPage:page3];
+    [self loadScrollViewWithPage:page4];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+-(void)viewDidAppear:(BOOL)animated
+
+{
+[super viewDidAppear:true];
+    
+    int pageCount = 4;
+    
+    _myScrollView.showsHorizontalScrollIndicator = YES;
+    _myScrollView.showsVerticalScrollIndicator = NO;
+    _myScrollView.delegate = self;
+    _myScrollView.contentSize = CGSizeMake(_myScrollView.frame.size.width * pageCount,
+                                           _myScrollView.frame.size.height);
+    
+    _myScrollView.pagingEnabled = true;
+    
+
+    
+    
+    _myPageControl.numberOfPages = pageCount;
+    _myPageControl.currentPage = 0;
+    [_myPageControl addTarget:self
+                       action:@selector(changePage:)
+             forControlEvents: UIControlEventValueChanged];
+    
+    
+    
+    
+    
+    // creat pages
+    [self createPages];
+}
+
+
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)sender
+{
+    CGFloat pageWidth = sender.frame.size.width;
+    int page = floor((sender.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    _myPageControl.currentPage = page;
+}
+
+
+
+
+
+
+-(void)changePage:(id)sender
+{
+    NSUInteger page = _myPageControl.currentPage;
+    
+    //update the sroll view to the appropriate page
+    CGRect frame = _myScrollView.frame;
+    frame.origin.x = frame.size.width * page;
+    frame.origin.y = 0;
+    [_myScrollView scrollRectToVisible:frame animated:YES];
 }
 
 
